7b6b334797fc1eee5ce6d14b3f27967c7944188c
jjy0918 <cua0918@naver.com>
2021-08-25 17:22:28 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/midas/epkorea/EpkoreaApplication.java
@@ -0,0 +1,13 @@
+package com.midas.epkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpkoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpkoreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/midas/epkorea/EpkoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.epkorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpkoreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
